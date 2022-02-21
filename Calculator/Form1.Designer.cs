74305bd887d8631a12711f0f83aff3138650f4d5
Suyog-Kulkarni <98457685+Suyog-Kulkarni@users.noreply.github.com>
2022-02-21 22:53:15 +0530

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.first = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.second = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // first
+            // 
+            this.first.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.first.BackColor = System.Drawing.SystemColors.Window;
+            this.first.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.first.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.first.ForeColor = System.Drawing.Color.Black;
+            this.first.Location = new System.Drawing.Point(407, 94);
+            this.first.Multiline = false;
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(153, 32);
+            this.first.TabIndex = 1;
+            this.first.Text = "";
+            this.first.TextChanged += new System.EventHandler(this.first_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(49, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter First Number : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(46, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(380, 42);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Second Number : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // second
+            // 
+            this.second.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.second.BackColor = System.Drawing.SystemColors.Window;
+            this.second.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.second.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.second.Location = new System.Drawing.Point(407, 170);
+            this.second.Multiline = false;
+            this.second.Name = "second";
+            this.second.Size = new System.Drawing.Size(153, 35);
+            this.second.TabIndex = 1;
+            this.second.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(54, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 53);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Turquoise;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.ImageKey = "(none)";
+            this.button2.Location = new System.Drawing.Point(454, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 54);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sub";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.Turquoise;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(54, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 53);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Mul";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Turquoise;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(454, 397);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(164, 53);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Div";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(767, 570);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.second);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.first);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox first;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox second;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        public int Letter_exception()
+        {
+
+            
+            for (int i = 0; i < first.TextLength; i++)
+            {
+                if (char.IsLetter(first.Text[i]))
+                {
+                    MessageBox.Show("Invalid Input!!!");
+                    first.Clear(); second.Clear(); first.Focus();
+                    return 0;
+                    
+                }
+                
+
+
+            }
+            for (int i = 0;i < second.TextLength; i++)
+            {
+                if (char.IsLetter(second.Text[i]))
+                {
+                    MessageBox.Show("Invalid Input!!!");
+                    first.Clear(); second.Clear(); first.Focus();
+                    return 0;
+
+                }
+            }
+            
+            return 1;
+        }
+
+
+            private void button1_Click_1(object sender, EventArgs e) 
+        {
+            first.Focus();
+
+            if (String.IsNullOrEmpty(first.Text) || String.IsNullOrWhiteSpace(first.Text) || String.IsNullOrEmpty(second.Text) || String.IsNullOrWhiteSpace(second.Text))
+            {
+                MessageBox.Show("One of the value is empty or both are empty.");
+                first.Clear(); second.Clear(); first.Focus();
+
+            }
+            if(Letter_exception() == 0) 
+            {
+                
+            }
+            else if(!String.IsNullOrEmpty(first.Text) && !String.IsNullOrEmpty(second.Text))
+            {
+                double c = Convert.ToDouble(first.Text.Trim()) + Convert.ToDouble(second.Text.Trim());
+                c.ToString();
+                MessageBox.Show("The Addition is :-  " + c);
+                first.Clear();
+                second.Clear();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            first.Focus();
+            if (String.IsNullOrEmpty(first.Text) || String.IsNullOrWhiteSpace(first.Text)|| String.IsNullOrEmpty(second.Text) || String.IsNullOrWhiteSpace(second.Text))
+            {
+                MessageBox.Show("One of the value is empty or both are empty.");
+                first.Clear(); second.Clear(); first.Focus();
+
+            }
+            if (Letter_exception() == 0)
+            { 
+
+            }
+            else if(!String.IsNullOrEmpty(first.Text) && !String.IsNullOrEmpty(second.Text))
+            { 
+
+                    double c = Convert.ToDouble(first.Text.Trim()) - Convert.ToDouble(second.Text.Trim());
+                    c.ToString();
+                    MessageBox.Show("The Substraction is :-  " + c);
+                    first.Clear();
+                    second.Clear();
+                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            first.Focus();
+
+            if (String.IsNullOrEmpty(first.Text) || String.IsNullOrWhiteSpace(first.Text) || String.IsNullOrEmpty(second.Text) || String.IsNullOrWhiteSpace(second.Text))
+            {
+                MessageBox.Show("One of the value is empty or both are empty.");
+                first.Clear(); second.Clear(); first.Focus();
+
+            }
+            if (Letter_exception() == 0) 
+            {
+
+            }
+            else if(!String.IsNullOrEmpty(first.Text) && !String.IsNullOrEmpty(second.Text))
+            {
+
+                double c = Convert.ToDouble(first.Text.Trim()) * Convert.ToDouble(second.Text.Trim());
+                c.ToString();
+                MessageBox.Show("The Multiplication is :-  " + c);
+                first.Clear();
+                second.Clear();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            first.Focus();
+            
+            if (String.IsNullOrEmpty(first.Text) || String.IsNullOrWhiteSpace(first.Text) || String.IsNullOrEmpty(second.Text) || String.IsNullOrWhiteSpace(second.Text))
+            {
+                MessageBox.Show("One of the value is empty or both are empty.");
+                first.Clear(); second.Clear(); first.Focus();
+
+            }
+            if (Letter_exception() == 0) 
+            {
+                
+            }
+            
+            else if(!String.IsNullOrEmpty(first.Text) && !String.IsNullOrEmpty(second.Text))
+            {
+                 
+                double c = Convert.ToDouble(first.Text.Trim()) / Convert.ToDouble(second.Text.Trim());
+                c.ToString();
+                MessageBox.Show("The Division is :-  " + c);
+                first.Clear();
+                second.Clear();
+            };
+        }
+
+        private void first_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
